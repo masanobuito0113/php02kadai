e27fd36c1532e61959ee8b748ae419bb654065fe
masanobuito0113 <japancatclub@gmail.com>
2023-06-25 14:53:43 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>データ登録</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <style>
+    div {
+        padding: 10px;
+        font-size: 16px;
+    }
+    </style>
+</head>
