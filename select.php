b5c628ff7034246c472c21c1e9601d72ee95aae2
masanobuito0113 <japancatclub@gmail.com>
2023-06-29 22:19:22 +0900

    ヘッダー追加

--- select.php
+++ select.php
@@ -40,9 +40,31 @@ if ($status==false){
 </head>
 
 <body>
-    <div class="container jumbotron"><?= $view ?></div>
 
-    <a class="navbar-brand" href="index.php">データ登録へもどる</a>
+
+    <main>
+        <h1 class="mt-3 mx-3">OMOHIDE ARCHIVE</h1>
+        <nav class="navbar navbar-expand-sm navbar-dark bg-dark mt-1 mb-1">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav4"
+                aria-controls="navbarNav4" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <a class="nav-link" href="select.php">OMOHIDE一覧</a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="index.php">新しく登録</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">お問い合わせ(停止中)</a>
+                </li>
+            </ul>
+            </div>
+        </nav>
+        <div class="container jumbotron"><?= $view ?></div>
+
+        <a class="navbar-brand" href="index.php">データ登録へもどる</a>
 
 </body>
 
