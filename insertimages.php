bb6dc5621a866cc3a116a930304ed854e11f29f9
masanobuito0113 <japancatclub@gmail.com>
2023-06-29 23:27:25 +0900

    画像アップできるかお試し中

--- insertimages.php
+++ insertimages.php
@@ -2,15 +2,14 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+// if ($_SERVER["REQUEST_METHOD"] == "POST") {
 $place = $_POST['place'];
-// $images = $_FILES['images']['name'];
 $food = $_POST['food'];
 $comment = $_POST['comment'];
 
-$imagePath = "path/to/htdocs/php02_table/images/";
-move_uploaded_file($images["tmp_name"], $imagePath);
-move_uploaded_file($_FILES['images']['tmp_name'], './upload/' . $images);
+// $imagePath = "path/to/htdocs/php02_table/images/";
+// move_uploaded_file($images["tmp_name"], $imagePath);
+// move_uploaded_file($_FILES['images']['tmp_name'], './upload/' . $images);
 
 try{
     $pdo = new PDO('mysql:dbname=gs_kadai_php02;charset=utf8;host=localhost','root','');
@@ -18,10 +17,15 @@ try{
     exit('DBConnectError:'.$e->getMessage());
 }
 
-$stmt = $pdo->prepare("INSERT INTO php02(id, place, food, comment, date ) VALUES (NULL, :place, :food, :comment, sysdate())");
+$upload_dir = 'upload/';
+
+$images = $_FILES['images']['tmp_name'];
+
+$stmt = $pdo->prepare("INSERT INTO php02(id, place, food, comment, images, date ) VALUES (NULL, :place, :food, :images :comment, sysdate())");
 $stmt->bindValue(':place', $place, PDO::PARAM_STR);
 $stmt->bindValue(':food', $food, PDO::PARAM_STR);
 $stmt->bindValue(':comment', $comment, PDO::PARAM_STR);
+$stmt->bindValue(':images', $comment, PDO::PARAM_STR);
 
 $status = $stmt->execute();
 
@@ -32,5 +36,5 @@ if($status === false){
     header('Location: index.php');
 
 }
-}
+// }
 ?>
